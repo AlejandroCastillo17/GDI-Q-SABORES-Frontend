e597a97807c12f3072ef9e0543c9ad79b16c8d42
JuanTB300405 <127706094+JuanTB300405@users.noreply.github.com>
2025-04-19 15:45:22 -0500

    correcion home

--- QsaboresFrontend/src/pages/Home.jsx
+++ QsaboresFrontend/src/pages/Home.jsx
@@ -1,18 +1,14 @@
 import "../styles/Home.css"
 import Buscador from "../components/buscador";
 import Button from "../components/Button";
-
-
 const Home = () => {
     return (
         <>
             <section className="home">
-              <h1>Venta</h1>
               <section className="contener-principal">
                 <Buscador/>
                 
                 <section className="principal_Contenido">
-                  <h1>aca van los productos</h1>
                 </section>
 
                 <section className="principal_valores">
@@ -25,14 +21,12 @@ const Home = () => {
                     <label htmlFor="devuelta">Devuelta</label>
                     <input type="number" id="devuelta" className="input_valor"/>
                 </section>
-
-                <section className="principal_botones">
+              </section>
+              <section className="principal_botones">
                     <Button variant="verde" id="aceptar">Aceptar</Button>
                     <Button variant="rojo" id="cancelar">Cancelar</Button>
-                </section>
-
-                
               </section>
+
             </section>
         </>
     )
