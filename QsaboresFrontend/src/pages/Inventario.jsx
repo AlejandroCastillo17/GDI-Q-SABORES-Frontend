61ca8c447332f5baee31ca1aef1d10eafda4423f
JuanTB300405 <127706094+JuanTB300405@users.noreply.github.com>
2025-05-09 17:12:48 -0500

    correcion inventario

--- QsaboresFrontend/src/pages/Inventario.jsx
+++ QsaboresFrontend/src/pages/Inventario.jsx
@@ -337,7 +337,7 @@ const Inventario = () => {
                             {(categoriaSeleccionada
                                 ? productosData.filter((p) => p.categoria === categoriaSeleccionada)
                                 : productosData
-                                ).map((producto, index) => 
+                                ).map((producto) => 
                                     <tr key={producto.id}>
                                         <td>
                                             <input 
@@ -379,7 +379,7 @@ const Inventario = () => {
                                                     />
                                                 ) 
                                                 : 
-                                                (producto.categoria)
+                                                (producto.categoria.nombre)
                                             }
                                         </td>
                                         <td>
@@ -499,7 +499,7 @@ const Inventario = () => {
                                             <select
                                                 id="select-form"
                                                 name="categoria"
-                                                value={datosForm.categoria}
+                                               
                                                 onChange={handleChange}
                                             >
                                                 <option value="ropa">Ropa</option>
