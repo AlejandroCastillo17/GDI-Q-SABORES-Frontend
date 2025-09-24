da18e6f8daa869dbb5c1405d97aa226bf1d5177c
JuanTB300405 <127706094+JuanTB300405@users.noreply.github.com>
2025-09-24 12:23:29 -0500

    d

--- src/js/login.js
+++ src/js/login.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const authentication = async (datos) => {
   try {
     const response = await axios.post(
-      "qsabores-backend-dacpsw-8612bd-72-60-26-170.traefik.me/sabores/api/v1/usuarios/login/",
+      "http://qsabores-backend-dacpsw-6d0816-72-60-26-170.dokploy.app/sabores/api/v1/usuarios/login/",
       datos,
       {
         headers: {
@@ -11,7 +11,6 @@ export const authentication = async (datos) => {
         },
       }
     );
-    //console.log('Respuesta del backend:', response);
     if (response.status === 200) {
       // Guardar el token en el almacenamiento local
       sessionStorage.setItem("token", response.data.token);
