708f5a449bab73838d27b5fde21c431fb61ac962
JuanTB300405 <127706094+JuanTB300405@users.noreply.github.com>
2025-09-24 12:12:52 -0500

    a

--- src/js/login.js
+++ src/js/login.js
@@ -1,21 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const authentication = async (datos) => {
-
   try {
-    const response = await axios.post('http://127.0.0.1:8000/sabores/api/v1/usuarios/login/', datos, {
-      headers: {
-        'Content-Type': 'application/json'
+    const response = await axios.post(
+      "http://qsabores-backend-<some-id>.dokploy.app/sabores/api/v1/usuarios/login/",
+      datos,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
       }
-    });
+    );
     //console.log('Respuesta del backend:', response);
     if (response.status === 200) {
       // Guardar el token en el almacenamiento local
-      sessionStorage.setItem('token', response.data.token);
+      sessionStorage.setItem("token", response.data.token);
     }
     return response;
   } catch (error) {
-    console.error('Error al enviar datos:', error.response?.data || error.message);
+    console.error(
+      "Error al enviar datos:",
+      error.response?.data || error.message
+    );
     return error.response || error.message;
   }
 };
